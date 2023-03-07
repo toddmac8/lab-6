66b8ee345d3147e498712214fcda4440ab7ffe3f
toddmac8 <toddmac8@gmail.com>
2023-03-07 15:57:19 -0500

    demonstration

---
+++ main.py
@@ -0,0 +1,16 @@
+def encode(pass_w):
+    pass
+def main():
+    program = True
+    while program:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        option = int(input("Please enter an option: "))
+        if option == 1:
+           pass_w = inp(Please enter your password to encode:)
+            print(encode(pass_w))
+
+if __name__ == '__main__':
