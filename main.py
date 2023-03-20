a014a88d2ae1a52bd6c9f918ad0d8214cbe99a44
toddmac8 <toddmac8@gmail.com>
2023-03-20 17:19:16 -0400

    finished

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-def encode(pass_w):
-    pass
+def encode(pass_wd):
+    pass_w = list(pass_wd)
+    pass_w_new = ""
+    for num in pass_w:
+        num = int(num) + 3
+        pass_w_new += str(num)
+    return pass_w_new
+
 def main():
     program = True
+    pass_wd = ""
+    pass_w_new = ""
     while program:
         print("Menu")
         print("-------------")
@@ -10,7 +18,18 @@ def main():
         print("3. Quit")
         option = int(input("Please enter an option: "))
         if option == 1:
-           pass_w = inp(Please enter your password to encode:)
-            print(encode(pass_w))
+           pass_wd = input("Please enter your password to encode: ")
+           pass_w_new = encode(pass_wd)
+           print("Your password has been encoded and stored!")
+
+        elif option == 2:
+            print(f"The encoded password is {pass_w_new}, and the original password is {pass_wd}.")
+
+        elif option == 3:
+            program = False
+
+
+
 
 if __name__ == '__main__':
+    main()
